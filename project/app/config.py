c5de7e8da18989edaa7f39fa6cfe580faa8d95bd
kytewy <wyattkyte@hotmail.com>
2022-02-12 23:34:55 -0500

    Adding database

--- project/app/config.py
+++ project/app/config.py
@@ -2,19 +2,19 @@ import logging
 import os
 from functools import lru_cache
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
 
 
 log = logging.getLogger("uvicorn")
 
 # BaseSettings validates the data so that when we create an instance of Settings
 # environment and testing will have types of str and bool, respectively.
-
 class Settings(BaseSettings):
     environment: str = os.getenv("ENVIRONMENT", "dev")
     testing: bool = os.getenv("TESTING", 0)
+    database_url: AnyUrl = os.environ.get("DATABASE_URL")
 
-# Caching the setting so it is not constantly being reloaded
+
 @lru_cache()
 def get_settings() -> BaseSettings:
     log.info("Loading config settings from the environment...")
